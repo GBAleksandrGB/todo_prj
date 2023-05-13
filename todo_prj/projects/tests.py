22a83bcc699d04937f380c5854ef5d5017e44ce9
GBAleksandrGB <tomilov.alex@gmail.com>
2023-05-13 13:08:17 +0300

    lesson 6

--- todo_prj/projects/tests.py
+++ todo_prj/projects/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+import json
+import requests
+from rest_framework import status
 
-# Create your tests here.
+response_token = requests.post(url='http://127.0.0.1:8000/api-token-auth/',
+                               data={'username': 'admin', 'password': '123'})
+assert response_token.status_code == status.HTTP_200_OK
+
+response_JWT = requests.post(url='http://127.0.0.1:8000/api/token/',
+                             headers={'content-type': 'application/json'},
+                             data=json.dumps({"username": "admin", "password": "123"}))
+assert response_token.status_code == status.HTTP_200_OK
+
+response_some_protect = requests.get(url='http://127.0.0.1:8000/projects/1/',
+                                     headers={'Authorization': f'Bearer {response_JWT.json().get("access")}'})
+assert response_token.status_code == status.HTTP_200_OK
+
+response_refresh = requests.post(url='http://127.0.0.1:8000/api/token/refresh/',
+                                 headers={'content-type': 'application/json'},
+                                 data=json.dumps({'refresh': response_JWT.json().get('refresh')}))
+assert response_token.status_code == status.HTTP_200_OK
+
